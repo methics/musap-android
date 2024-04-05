0422e148d32e03b4c15be02cd656c9321165e413
Atte <waldenatte@gmail.com>
2024-04-05 10:50:45 +0300

    Add tests

---
+++ app/src/test/java/fi/methics/musap/sdk/internal/util/HexUtilTest.java
@@ -0,0 +1,24 @@
+package fi.methics.musap.sdk.internal.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HexUtilTest {
+
+    @Test
+    public void hexLine() {
+    }
+
+    @Test
+    public void testHexLine() {
+    }
+
+    @Test
+    public void testHexLine1() {
+    }
+
+    @Test
+    public void parseHex() {
+    }
+}
